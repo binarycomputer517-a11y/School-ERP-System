cddb894ab5b6c1e9bb751e2d52fb3de78f7eb68d
Sudam <sudammaity@Sudams-MacBook-Air.local>
2025-11-07 08:38:51 +0530

    HOTFIX: Changed require('bcrypt') to require('bcryptjs') in routes/teachers.js

--- routes/teachers.js
+++ routes/teachers.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const { pool } = require('../database');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 const { authenticateToken, authorize } = require('../authMiddleware');
 
