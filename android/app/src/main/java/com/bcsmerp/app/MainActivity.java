947ccd52b97ce6aa72660910cf1d5bc4289c627a
Sudam <sudammaity@Sudams-MacBook-Air.local>
2026-02-17 11:57:42 +0530

    Update: English ID Lookup and UI fixes

--- android/app/src/main/java/com/bcsmerp/app/MainActivity.java
+++ android/app/src/main/java/com/bcsmerp/app/MainActivity.java
@@ -1,23 +1,5 @@
 package com.bcsmerp.app;
 
-import android.os.Bundle;
-import android.webkit.PermissionRequest;
-import android.webkit.WebChromeClient;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // This allows the Android WebView to handle the JavaScript 
-        // camera/microphone requests (navigator.mediaDevices.getUserMedia)
-        this.bridge.getWebView().setWebChromeClient(new WebChromeClient() {
-            @Override
-            public void onPermissionRequest(final PermissionRequest request) {
-                // This line tells Android to grant the permission requested by the web page
-                request.grant(request.getResources());
-            }
-        });
-    }
-}
+public class MainActivity extends BridgeActivity {}
